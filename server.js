488a54cce1d046ef79bd95af4a768ce391197682
namu5 <code@kakao.com>
2021-02-15 10:11:02 +0900

    #3.3 typeDefs, resolvers 변경으로 Movie API 테스트

--- server.js
+++ server.js
@@ -1,14 +1,35 @@
 import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
+  type Movie {
+    title: String
+    year: Int
+  }
   type Query {
-    hello: String
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "hiiiiiiiiiiiiiii",
+    movies: () => [],
+    movie: () => ({ title: "test", year: "2021" }),
+  },
+
+  Mutation: {
+    createMovie: (_, {title}) => {
+      console.log(title);
+      return true;
+    },
+    deleteMovie: (_, {title}) => {
+      console.log(title);
+      return true;
+    },
   },
 };
 
