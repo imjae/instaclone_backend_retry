c7b6741926d8819fcd278f0b3de26b6d93131782
namu5 <code@kakao.com>
2021-02-19 10:32:49 +0900

    createAcconut 뮤테이션 생성(검증,password해싱), README파일작성

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # Instaclone
 
 Instaclone Backend Retry
+
+## User:
+- [ ] Create Account
+- [ ] See Profile
+- [ ] Login
+- [ ] Edit Profile
+- [ ] Change Avatar (Image Upload)
+- [ ] Follow User
+- [ ] Unfollow User
+- [ ] Search Users
+- [ ] See Followers
+- [ ] See Following
+
+## Photos:
+- [ ] See Photo
+- [ ] Upload Photo
+- [ ] Edit Photo
+- [ ] Like / Unlike Photo
+- [ ] See Photo Likes
+- [ ] See Feed
+- [ ] Search Photos
+- [ ] See Hashtags
+
+## Comments:
--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,5 +1,37 @@
+import client from '../client';
+import bcrypt from 'bcrypt';
+
 export default {
   Mutation: {
-    createAccount: (_, args) => console.log('createAccount mutation!'),
+    createAccount: async (_, { firstName, lastName, userName, email, password }) => {
+      // DB에 username과 email이 존재하는지 확인
+      const existinguser = await client.user.findFirst({
+        where: {
+          OR: [{ userName }, { email }],
+        },
+      });
+      // console.log(existinguser);
+
+      try {
+        if (!existinguser) {
+          // password hash
+          const uglyPassword = await bcrypt.hash(password, 10);
+
+          return await client.user.create({
+            data: {
+              firstName,
+              lastName,
+              userName,
+              email,
+              password: uglyPassword,
+            },
+          });
+        } else {
+          throw new Error('userName, email이 이미 존재합니다.');
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    },
   },
 };
