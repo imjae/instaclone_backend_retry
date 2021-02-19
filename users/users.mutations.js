8043adbe8d87987b3ea40c9edb92c9dd9832e937
namu5 <code@kakao.com>
2021-02-19 10:45:03 +0900

    existingUser 검증로직 변경

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -5,32 +5,31 @@ export default {
   Mutation: {
     createAccount: async (_, { firstName, lastName, userName, email, password }) => {
       // DB에 username과 email이 존재하는지 확인
-      const existinguser = await client.user.findFirst({
+      const existingUser = await client.user.findFirst({
         where: {
           OR: [{ userName }, { email }],
         },
       });
-      // console.log(existinguser);
+      // console.log(existingUser);
 
       try {
-        if (!existinguser) {
-          // password hash
-          const uglyPassword = await bcrypt.hash(password, 10);
-
-          return await client.user.create({
-            data: {
-              firstName,
-              lastName,
-              userName,
-              email,
-              password: uglyPassword,
-            },
-          });
-        } else {
+        if (existingUser) {
           throw new Error('userName, email이 이미 존재합니다.');
         }
+        // password hash
+        const uglyPassword = await bcrypt.hash(password, 10);
+
+        return await client.user.create({
+          data: {
+            firstName,
+            lastName,
+            userName,
+            email,
+            password: uglyPassword,
+          },
+        });
       } catch (e) {
-        console.log(e);
+        return e;
       }
     },
   },
